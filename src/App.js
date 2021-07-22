15a977b3487a95cb5dfcaf51deb95030519339b1
AlenaLis <87519836+AlenaLis@users.noreply.github.com>
2021-07-22 20:10:07 +0300

    Revert "Main page"

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
-import './App.scss';
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import React, {useEffect, useState} from 'react';
-import {BrowserRouter, Switch, Route} from "react-router-dom";
-import {routes} from "./routes/routes";
-import {blogs} from "./assets/Services/mock";
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-
+function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <div className="wrapper">
-          <div className="wrapper__header">
-            <Header/>
-          </div>
-          <Switch>
-            <div className="wrapper__all">
-              {routes.map((route, index) => (
-                <Route
-                  key={index}
-                  path={route.path}
-                  exact={route.exact}
-                  component={route.component}
-                />
-              ))}
-            </div>
-          </Switch>
-          <div className="wrapper__footer">
-            <Footer/>
-          </div>
-        </div>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
